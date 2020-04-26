9e6ac4589a5d6f8deb4ca726a9eb1445114fb80e
Marcelo <marcelo@marcelos-pro.home>
2020-04-26 11:19:48 -0300

    'Latest Activities' added and Article model updated

--- articles/models.py
+++ articles/models.py
@@ -7,11 +7,12 @@ from django.utils import timezone
 class Article(models.Model):
 	title = models.CharField(max_length=50)
 	text = models.CharField(max_length=4000)
+	category = models.CharField(max_length=256, choices=[('Tech', 'Tech'), ('Games', 'Games'), ('PC', 'PC'), ('Anarcho-capitalism', 'Anarcho-capitalism'), ('History', 'History'), ('News', 'News')])
 	pub_date = models.DateTimeField()
-	upd_date = models.DateTimeField()
+	upd_date = models.DateTimeField(null=True)
 	up_votes = models.IntegerField(default=0)
 	down_votes = models.IntegerField(default=0)
-	reference = models.CharField(max_length=200)
+	reference = models.CharField(max_length=200, null=True)
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 	def __str__(self):
--- home/views.py
+++ home/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import get_object_or_404, render
 
-#from .models import Article
+from articles.models import Article
 
 def index(request):
-#    latest_article_list = Article.objects.order_by('-pub_date')[:5]
-#    context = {'latest_article_list': latest_article_list}
-#    return render(request, 'articles/index.html', context)
-  	return render(request, 'home/index.html')
+	latest_article_list = Article.objects.order_by('-pub_date')[:6]
+	context = {'latest_article_list': latest_article_list}
+	return render(request, 'home/index.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#  	return render(request, 'home/index.html')
 
 
 #def article(request, article_id):
